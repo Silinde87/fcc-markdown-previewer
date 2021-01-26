81c027ad8c9cad7ad4cd2ffebf514a111b26d243
Silinde87 <paurodriguezmolina@gmail.com>
2021-01-26 19:02:23 +0100

    Closes #2; Added maximize feature

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function parseIt(){
     marked(editor);
 }
 
-
+$(".btn-maximize").click(function() {
+    let divToMaximize = "#" + $(this).parent().parent().attr('id');
+    let divToHide = "#previewWrap";
+    console.log($(this).parent().parent().attr('id'));
+    if(divToMaximize === "#previewWrap"){
+        divToHide = "#editorWrap"
+        console.log("in");
+    }
+    $(divToMaximize).toggleClass("maximized");
+    $(divToHide).toggleClass("hide");
+  });
 
 
